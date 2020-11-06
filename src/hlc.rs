15dae6357fbf23757fd2434983bdc481f1acce26
Dan Bruder <danb@structionsite.com>
2020-11-05 22:37:54 -0500

    wip

--- src/hlc.rs
+++ src/hlc.rs
@@ -1,13 +1,14 @@
 use chrono::{TimeZone, Utc};
+use std::cmp::max;
 
-struct Hlc {
+struct Timestamp {
     millis: i64,
-    counter: i16,
-    node: i16,
+    counter: i64,
+    node: i64,
 }
 
-impl Hlc {
-    pub fn new(millis: i64, counter: i16, node: i16) -> Self {
+impl Timestamp {
+    pub fn new(millis: i64, counter: i64, node: i64) -> Self {
         Self {
             millis,
             counter,
@@ -23,6 +24,29 @@ impl Hlc {
             self.node
         )
     }
+
+    pub fn send(&mut self) -> Result<(), String> {
+        let phys = Utc::now().timestamp_millis();
+
+        let l_old = self.millis;
+        let c_old = self.counter;
+
+        let l_new = max(l_old, phys);
+        let c_new = if l_old == l_new { c_old + 1 } else { 0 };
+
+        if l_new - phys > 60000 {
+            return Err("Clockdrift!".to_string());
+        }
+
+        if c_new > 65535 {
+            return Err("overflow".to_string());
+        }
+
+        self.millis = l_new;
+        self.counter = c_new;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -31,7 +55,7 @@ mod test {
 
     #[test]
     fn test_to_string() {
-        let ts = Hlc::new(0, 0, 0);
+        let ts = Timestamp::new(0, 0, 0);
         let got = ts.to_string();
         let want = "1970-01-01T00:00:00+00:00-0000-0000000000000000";
         assert_eq!(got, want);
